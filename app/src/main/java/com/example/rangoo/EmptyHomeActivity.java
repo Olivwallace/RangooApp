5c434348208ab42aee98309d6dc6edc2139f0c23
Gabriella <2022954186@teiacoltec.org>
2023-06-23 20:28:49 -0300

    upload

---
+++ app/src/main/java/com/example/rangoo/EmptyHomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.rangoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmptyHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_home);
+    }
+}
