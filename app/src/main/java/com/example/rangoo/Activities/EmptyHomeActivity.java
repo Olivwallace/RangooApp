4716944e7405f956d47eb1dbcad73d2e7a5d8b69
Gabriella <2022954186@teiacoltec.org>
2023-06-27 11:19:56 -0300

    nav bar + menu

--- app/src/main/java/com/example/rangoo/Activities/EmptyHomeActivity.java
+++ app/src/main/java/com/example/rangoo/Activities/EmptyHomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.rangoo.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.rangoo.R;
 
@@ -12,5 +16,14 @@ public class EmptyHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty_home);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ImageButton btn_openDrawer = findViewById(R.id.btn_open_drawer);
+
+        btn_openDrawer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
     }
 }
